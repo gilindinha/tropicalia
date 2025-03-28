23a2b3c46441a0272ad8a0f05f0151612371bc3c
gilindinha <142936750+gilindinha@users.noreply.github.com>
2025-03-28 16:13:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function(){
@@ -22,6 +24,10 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.toggle('alto-contraste')
     })
 
 
